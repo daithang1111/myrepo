54465cdaccc9642e639885264deb2ac1e83d5c62
daithang <daithang@umiacs.umd.edu>
2014-08-27 22:58:24 -0400

    Added HW# from class

--- src/main/java/tn/util/Template.java
+++ src/main/java/tn/util/Template.java
@@ -1,10 +1,183 @@
 package tn.util;
 
+/**
+ * @author nguyentd4
+ * A template for a word count program (extracted from @lintool
+ */
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.StringTokenizer;
+
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.GnuParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.OptionBuilder;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.util.Tool;
 import org.apache.hadoop.util.ToolRunner;
+import org.apache.log4j.Logger;
+
+import cern.colt.Arrays;
 
 public class Template extends Configured implements Tool {
+
+	private static final Logger LOG = Logger.getLogger(Template.class);
+
+	/**
+	 * mapper
+	 * 
+	 * @author nguyentd4
+	 * 
+	 */
+	private static class MyMapper extends
+			Mapper<LongWritable, Text, Text, IntWritable> {
+
+		private static final IntWritable ONE = new IntWritable(1);
+		private static final Text WORD = new Text();
+
+		@Override
+		public void map(LongWritable key, Text value, Context context)
+				throws IOException, InterruptedException {
+			String line = ((Text) value).toString();
+			StringTokenizer token = new StringTokenizer(line);
+
+			String tokenStr = "";
+			while (token.hasMoreTokens()) {
+				tokenStr = token.nextToken();
+				if (tokenStr.matches("[A-Za-z]+")) {
+					WORD.set(tokenStr);
+					context.write(WORD, ONE);
+				}
+			}
+		}
+	}
+
+	/**
+	 * myreducer
+	 * @author nguyentd4
+	 *
+	 */
+	private static class MyReducer extends
+			Reducer<Text, IntWritable, Text, IntWritable> {
+
+		private static final IntWritable SUM = new IntWritable();
+
+		@Override
+		public void reduce(Text key, Iterable<IntWritable> values,
+				Context context) throws IOException, InterruptedException {
+
+			Iterator<IntWritable> iter = values.iterator();
+
+			int sum = 0;
+			while (iter.hasNext()) {
+
+				sum += iter.next().get();
+			}
+
+			SUM.set(sum);
+			context.write(key, SUM);
+		}
+
+	}
+
+	// contructor
+	public Template() {
+	};
+
+	// input, output, numreducer
+	private static final String INPUT = "input";
+	private static final String OUTPUT = "output";
+	private static final String NUM_REDUCERS = "numReducers";
+
+	@SuppressWarnings("static-access")
+	@Override
+	public int run(String[] args) throws Exception {
+
+		// set options
+		Options options = new Options();
+		options.addOption(OptionBuilder.withArgName("path").hasArg()
+				.withDescription("input path").create(INPUT));
+		options.addOption(OptionBuilder.withArgName("path").hasArg()
+				.withDescription("output path").create(OUTPUT));
+		options.addOption(OptionBuilder.withArgName("num").hasArg()
+				.withDescription("number of reducers").create(NUM_REDUCERS));
+
+		// command line
+		CommandLine cmdline;
+		CommandLineParser parser = new GnuParser();
+
+		try {
+			cmdline = parser.parse(options, args);
+		} catch (ParseException exp) {
+			System.err.println("Error parsing command line: "
+					+ exp.getMessage());
+			return -1;
+		}
+		if (!cmdline.hasOption(INPUT) || !cmdline.hasOption(OUTPUT)) {
+			System.out.println("args: " + Arrays.toString(args));
+			HelpFormatter formatter = new HelpFormatter();
+			formatter.setWidth(120);
+			formatter.printHelp(this.getClass().getName(), options);
+			ToolRunner.printGenericCommandUsage(System.out);
+			return -1;
+		}
+		String inputPath = cmdline.getOptionValue(INPUT);
+		String outputPath = cmdline.getOptionValue(OUTPUT);
+		int reduceTasks = cmdline.hasOption(NUM_REDUCERS) ? Integer
+				.parseInt(cmdline.getOptionValue(NUM_REDUCERS)) : 1;
+		LOG.info("Tool: " + Template.class.getSimpleName());
+		LOG.info(" - input path: " + inputPath);
+		LOG.info(" - output path: " + outputPath);
+		LOG.info(" - number of reducers: " + reduceTasks);
+
+		// create job
+		Configuration conf = getConf();
+		Job job = Job.getInstance(conf);
+
+		// set name, jar
+		job.setJobName(Template.class.getSimpleName());
+		job.setJarByClass(Template.class);
+
+		// set numreducers
+		job.setNumReduceTasks(reduceTasks);
+
+		FileInputFormat.setInputPaths(job, new Path(inputPath));
+		FileOutputFormat.setOutputPath(job, new Path(outputPath));
+
+		job.setOutputKeyClass(Text.class);
+		job.setOutputValueClass(IntWritable.class);
+
+		job.setMapperClass(MyMapper.class);
+		job.setCombinerClass(MyReducer.class);
+		job.setReducerClass(MyReducer.class);
+
+		// delete output directory if exists
+		Path outputDir = new Path(outputPath);
+		FileSystem.get(conf).delete(outputDir, true);
+
+		long startTime = System.currentTimeMillis();
+
+		job.waitForCompletion(true);
+
+		LOG.info("Job finished in " + (System.currentTimeMillis() - startTime)
+				/ 1000.0 + " seconds");
+		return 0;
+
+	}
 
 	/**
 	 * Dispatches command-line arguments to the tool via the {@code ToolRunner}.
@@ -13,9 +186,4 @@ public class Template extends Configured implements Tool {
 		ToolRunner.run(new Template(), args);
 	}
 
-	@Override
-	public int run(String[] arg0) throws Exception {
-		// TODO Auto-generated method stub
-		return 0;
-	}
 }
