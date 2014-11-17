07e4bdbc7e8e451eebd9b34369feaa66eddf1e5d
daithang1111 <daithang1111@gmail.com>
2014-11-17 08:30:54 -0500

    add dll, so file for svm light

--- README.md
+++ README.md
@@ -5,3 +5,4 @@ We should write a read me file here.
 
 Always run this in order to use svm light:
 mvn install:install-file -Dfile=lib/jnisvmlight.jar -DgroupId=tn -DartifactId=svmlight -Dversion=6.1 -Dpackaging=jar
+And need to point java path to the lib
--- src/main/java/tn/test/Test.java
+++ src/main/java/tn/test/Test.java
@@ -1,50 +1,12 @@
 package tn.test;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.Statement;
-import java.util.Calendar;
 
 public class Test {
 
 	public static void main(String[] args) {
-		String abc ="this#is#nice";
-		System.out.println(abc.split("#").length);
-		
-		long ltime = 1074912436;
-		
-		Calendar cal =Calendar.getInstance();
-		cal.setTimeInMillis(1074912436000L);
-		
-		System.out.println(cal.getTime().toString());
-		
-		
-		
-		
-		
-		
-		String x = "this\nis \tfun";
-		String values = x.replaceAll("[\\r\\n\\t]+", " ");
-		System.out.println(values);
+		String path = System.getProperty("java.library.path");
+		System.out.println(path);
 
-	}
-
-	public static void createTable(String database, String sqlScript) {
-		Connection c = null;
-		Statement stmt = null;
-		try {
-			Class.forName("org.sqlite.JDBC");
-			c = DriverManager.getConnection("jdbc:sqlite:" + database);
-
-			stmt = c.createStatement();
-
-			stmt.executeUpdate(sqlScript);
-			stmt.close();
-			c.close();
-		} catch (Exception e) {
-			System.err.println(e.getClass().getName() + ": " + e.getMessage());
-			System.exit(0);
-		}
 	}
 
 }
